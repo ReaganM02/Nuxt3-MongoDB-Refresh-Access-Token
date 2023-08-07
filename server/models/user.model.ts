9796731660551cf0b6a56ec37f96af54365a6a18
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-07 21:23:33 +0800

    Created a user schema/model

---
+++ server/models/user.model.ts
@@ -0,0 +1,75 @@
+import mongoose from 'mongoose'
+import { UserSchema } from '../../types/user'
+
+const userSchema = new mongoose.Schema<UserSchema>({
+    uuid: {
+        type: Number,
+        required: [true, 'UUID is required.'],
+        index: {
+            unique: true
+        }
+    },
+    firstName: {
+        type: String,
+        required: [true, 'First name is required.'],
+        trim: true,
+        validate: {
+            validator: function (value: string) {
+                return /^[a-zA-Z\s-]+$/.test(value)
+            },
+            message: 'Invalid First Name'
+        }
+    },
+    lastName: {
+        type: String,
+        required: [true, 'First name is required.'],
+        trim: true,
+        validate: {
+            validator: function (value: string) {
+                return /^[a-zA-Z\s-]+$/.test(value)
+            },
+            message: 'Invalid First Name'
+        }
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required.'],
+        validate: {
+            validator: function (value: string) {
+                return /^\S+@\S+\.\S+$/.test(value)
+            },
+            message: 'Invalid email.'
+        },
+        index: {
+            unique: true
+        }
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required.'],
+        select: false,
+        minlength: [10, 'Password is too short.']
+    },
+    verificationCode: {
+        type: String,
+        select: false
+    },
+    verificationCodeExpAt: {
+        type: Date,
+        select: false
+    },
+    resetPassword: {
+        type: String,
+        select: false
+    },
+    resetPasswordExpAt: {
+        type: Date,
+        select: false
+    }
+})
+
+export default mongoose.model<UserSchema>('User', userSchema)
