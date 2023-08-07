55a1f3cb545199a963fe9953c094db3b8daef4df
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-07 21:24:15 +0800

    Created a sign up functionality

---
+++ pages/sign-up.vue
@@ -0,0 +1,119 @@
+<script lang="ts" setup>
+import { FormKitNode } from '@formkit/core'
+import { NuxtError } from 'nuxt/app'
+import { UserRequestBody } from 'types/user'
+
+definePageMeta({
+    layout: 'user'
+})
+
+const showPassword = ref(false)
+const loading = ref(false)
+
+async function onCreateAccount(data: UserRequestBody, node: FormKitNode | undefined) {
+    loading.value = true
+    node?.clearErrors()
+    await $fetch('/api/user/sign-up', {
+        method: 'POST',
+        body: data
+    }).then(async (data) => {
+        await navigateTo(`/account-verification/${data}`)
+    }).catch((err: { data: NuxtError }) => {
+        const formErrorMessage = 'Failed to create an account.'
+        const error = err.data
+        if (error.statusCode !== 200) {
+            if (Array.isArray(error.data)) {
+                error.data.forEach(e => node?.setErrors(formErrorMessage, e))
+            } else if (error.statusCode === 500) {
+                node?.setErrors(formErrorMessage, error.statusMessage)
+            } else if (error.statusCode === 409) {
+                node?.setErrors(formErrorMessage, {
+                    email: error.statusMessage!
+                })
+            } else {
+                node?.setErrors(formErrorMessage, error.statusMessage)
+            }
+        }
+    }).finally(() => {
+        loading.value = false
+    })
+}
+
+</script>
+<template>
+    <div>
+        <AccountTitle label="Create a New Account" />
+        <p class="mt-2 text-zinc-500 text-center">
+            Sign up now to access exclusive features and personalized experiences.It's quick, and easy.
+        </p>
+        <div class="mt-6">
+            <FormKit
+                type="form"
+                :actions="false"
+                @submit="onCreateAccount"
+            >
+                <div class="flex gap-4">
+                    <div class="w-full">
+                        <FormKit
+                            type="text"
+                            label="First Name"
+                            placeholder="John"
+                            name="firstName"
+                            validation="required:trim|alpha_spaces:latin"
+                        />
+                    </div>
+                    <div class="w-full">
+                        <FormKit
+                            type="text"
+                            label="Last Name"
+                            placeholder="Wick"
+                            name="lastName"
+                            validation="required:trim|alpha_spaces:latin"
+                        />
+                    </div>
+                </div>
+                <!-- Email -->
+                <div class="mt-6">
+                    <FormKit
+                        type="email"
+                        label="Email"
+                        placeholder="johnwick@gmail.com"
+                        name="email"
+                        validation="required:trim|email"
+                    />
+                </div>
+                <!-- Password -->
+                <div class="mt-6">
+                    <FormKit
+                        :type="showPassword ? 'text' : 'password'"
+                        label="Password"
+                        :placeholder="showPassword ? 'Password12345' : '***********'"
+                        name="password"
+                        help="Min of 10 characters"
+                        validation="required:trim|length:10,30"
+                    />
+                    <AccountPasswordToggle
+                        :is-visible="showPassword"
+                        @click.prevent="showPassword = !showPassword"
+                    />
+                </div>
+                <!-- Submit -->
+                <div class="mt-8">
+                    <AccountFormSubmit
+                        :loading="loading"
+                        label="Create Account"
+                    />
+                </div>
+            </FormKit>
+            <div class="mt-6 text-center text-zinc-500">
+                Already have an account? <NuxtLink
+                    class="text-blue-600 hover:underline"
+                    to="/"
+                >
+                    Sign in
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="css"></style>
--- server/api/user/sign-up.post.ts
+++ server/api/user/sign-up.post.ts
@@ -1,7 +1,47 @@
+import { Error } from 'mongoose'
+import { MongoError } from 'mongodb'
+import { UserRequestBody } from '../../../types/user'
+import userModel from '../../models/user.model'
+
 export default defineEventHandler(async (event) => {
     try {
-        const test = await useUserModel.findOne()
-    } catch (error) {
+        const body = await readBody<UserRequestBody | null>(event)
+        if (!body) {
+            return createError({ statusCode: 400, statusMessage: 'Bad request.' })
+        }
+        const verificationCode = generateUuid(6)
+        const user = await userModel.create({
+            uuid: generateUuid(19),
+            firstName: sanitize(body.firstName),
+            lastName: sanitize(body.lastName),
+            email: sanitize(body.email),
+            password: await hashStrings(body.password),
+            verificationCode: await hashStrings(verificationCode),
+            verificationCodeExpAt: new Date(Date.now() + 10 * 60 * 1000) // 10 minutes
+        })
+        const emailTemplate = await getEmailTemplate('authentication-account-verification')
+        if (!emailTemplate) {
+            return createError({ statusCode: 500, statusMessage: 'Apologies, an internal server error while sending verification email. Please contact development team.' })
+        }
+        const sendAnEmail = await sendEmail({
+            body: emailTemplate,
+            email: [user.email],
+            mergeFields: { firstName: user.firstName, code: verificationCode },
+            from: `Reagan M <${useRuntimeConfig().EMAIL}>`,
+            replyTo: `Reagan M <${useRuntimeConfig().EMAIL}>`,
+            subject: 'Account Verification'
+        })
+        if (!sendAnEmail) {
+            return createError({ statusCode: 500, statusMessage: 'Apologies, an internal server error is hindering the verification email sending. Please contact our development team.' })
+        }
+        return user.uuid
+    } catch (error: unknown) {
+        if (error instanceof Error.ValidationError) {
+            return createError({ statusCode: 400, statusMessage: 'Bad request', data: Object.values(error.errors).map(err => ({ [err.path]: err.message })) })
+        }
+        if (error instanceof MongoError) {
+            return createError({ statusCode: 409, statusMessage: 'Email already exist' })
+        }
         return createError({ statusCode: 500, statusMessage: 'Something went wrong' })
     }
 })
