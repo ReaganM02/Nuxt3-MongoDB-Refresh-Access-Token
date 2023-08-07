4e7def8e950dc9646ff2ae73cf3af10ef0605b55
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-07 21:27:53 +0800

    Added runtime configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
   modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss'],
   runtimeConfig: {
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    TOKEN_SECRET: process.env.TOKEN_SECRET,
+    TOKEN_REFRESH: process.env.TOKEN_REFRESH,
+    ELASTIC_API: process.env.ELASTIC_API,
+    EMAIL: 'reagansoftwaredev@gmail.com',
+    public: {
+      ELASTIC_URL: 'https://api.elasticemail.com/v4'
+    }
   },
-  imports: {
-    dirs: ['server/models']
+  nitro: {
+    plugins: ['~/server/plugins/mongoDB.ts']
   }
 })
