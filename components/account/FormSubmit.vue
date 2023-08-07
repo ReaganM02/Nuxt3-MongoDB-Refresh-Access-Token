74b63005e2dfa8150beffb6c44211ce56784c868
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-07 21:24:49 +0800

    Created a reusable form submit

---
+++ components/account/FormSubmit.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps<{
+    label: string
+    loading: boolean
+}>()
+</script>
+<template>
+    <FormKit type="submit">
+        <span
+            v-if="loading"
+            class="absolute w-full bg-blue-600 top-0 left-0 bottom-0 right-0 grid place-items-center"
+        >
+            <IconSpinner class="w-5 h-5" />
+        </span>
+        <span v-else>{{ label }}</span>
+    </FormKit>
+</template>
+<style scoped lang="css"></style>
