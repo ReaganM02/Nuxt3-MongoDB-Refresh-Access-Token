77ae41f2a85596a5b7908926c735411ea92db363
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-08 00:44:01 +0800

    Added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,16 +7,29 @@ export default defineNuxtConfig({
       enabled: true
     }
   },
-  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss'],
+  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss', ['@pinia/nuxt', {
+    autoImports: [
+      'acceptHMRUpdate',
+      'defineStore',
+      ['defineStore', 'definePiniaStore']
+    ]
+  }]],
+  imports: {
+    dirs: ['stores']
+  },
   runtimeConfig: {
     MONGO_URI: process.env.MONGO_URI,
     TOKEN_SECRET: process.env.TOKEN_SECRET,
     TOKEN_REFRESH: process.env.TOKEN_REFRESH,
     ELASTIC_API: process.env.ELASTIC_API,
+    APP_URL: 'http://localhost:3000',
     EMAIL: 'reagansoftwaredev@gmail.com',
     public: {
       ELASTIC_URL: 'https://api.elasticemail.com/v4'
     }
+  },
+  alias: {
+    pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs'
   },
   nitro: {
     plugins: ['~/server/plugins/mongoDB.ts']
