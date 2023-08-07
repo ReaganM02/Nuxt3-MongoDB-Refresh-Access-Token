cefcd446083cc4c3cad640dc3527548703100281
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-07 21:25:56 +0800

    Created a user layout

---
+++ layouts/user.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <main class="h-screen">
+        <div class="h-screen grid grid-cols-2">
+            <div class="bg-gradient-to-br from-sky-700 via-violet-500 to-red-700">
+                <!-- -->
+            </div>
+            <div class="grid place-items-center">
+                <div class="max-w-lg w-full">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+<style scoped lang="css"></style>
