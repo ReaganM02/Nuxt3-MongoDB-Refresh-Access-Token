04e65f89021cd1f2421a69cccde7853f79fb3629
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-07 21:25:40 +0800

    Created a reusable form title

---
+++ components/account/title.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+withDefaults(defineProps<{
+    size?: string
+    label: string
+}>(), {
+    size: 'text-2xl'
+})
+</script>
+<template>
+    <h1
+        class="text-center font-bold text-zinc-900"
+        :class="size"
+    >
+        {{ label }}
+    </h1>
+</template>
+<style scoped lang="css"></style>
