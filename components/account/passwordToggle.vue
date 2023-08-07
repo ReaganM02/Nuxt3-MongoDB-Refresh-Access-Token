20f0895a92038426baed7e17a1e951ce52baca55
ReaganM02 <reaganmahinay.web@gmail.com>
2023-08-07 21:25:03 +0800

    Created a reusable password toggle

---
+++ components/account/passwordToggle.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+defineProps<{
+    isVisible: boolean
+}>()
+</script>
+<template>
+    <div class="text-sm text-zinc-400 cursor-pointer float-right hover:underline hover:text-blue-600">
+        {{ isVisible ? 'Hide password' : 'Show password' }}
+    </div>
+</template>
+<style scoped lang="css"></style>
